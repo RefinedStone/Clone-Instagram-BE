18d10bd0024deb302049bd3de6a3011a09f82ce4
GGuiGui <rbqjachl95@gmail.com>
2022-10-30 00:59:56 +0900

    토요일 마지막 저장

--- src/main/java/com/example/cloneinstargram/CloneInstagramBE.java
+++ src/main/java/com/example/cloneinstargram/CloneInstagramBE.java
@@ -2,11 +2,10 @@ package com.example.cloneinstargram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CloneInstagramBE {
     public static void main(String[] args) {
         SpringApplication.run(CloneInstagramBE.class, args);

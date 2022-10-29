376f0a523a77c8f98739dcf5a05b57ee7d70772d
GGuiGui <97419217+GGuiGui@users.noreply.github.com>
2022-10-30 00:55:42 +0900

    상세보기 댓글의 시간 null 해결 EnableJpaAuditing 추가

--- src/main/java/com/example/cloneinstargram/CloneInstagramBE.java
+++ src/main/java/com/example/cloneinstargram/CloneInstagramBE.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerA
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CloneInstagramBE {
     public static void main(String[] args) {
         SpringApplication.run(CloneInstagramBE.class, args);
